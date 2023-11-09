76da64c7dafdad01ec86d823f9fa5a8a12b35a0e
babacar.adje <babacar.adje@intouchgroup.net>
2023-11-09 10:47:48 +0000

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  //API_URL:  "http://localhost:2020/api/v1",
-  //API_URL_ECOBANK: "http://localhost:2020/ecobank"
-  API_URL:  "https://dev-touch-ssii-api.gutouch.net/gateway/api/v1",
-  API_URL_ECOBANK: "https://dev-touch-ssii-api.gutouch.net/gateway/ecobank"
+  //API_URL:  "http://localhost:3030/api/v1",
+  //API_URL:  "https://dev-touch-ssii-api.gutouch.net/gateway/api/v1",
+  API_URL:  "https://prod-ssii.gutouch.net/gateway/api/v1"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  //API_URL:  "http://localhost:2020/api/v1"
-  API_URL:  "https://dev-touch-ssii-api.gutouch.net/gateway/api/v1"
+  //API_URL:  "http://localhost:3030/api/v1"
+  //API_URL:  "https://dev-touch-ssii-api.gutouch.net/gateway/api/v1"
+  API_URL:  "https://prod-ssii.gutouch.net/gateway/api/v1"
 };
 
 /*
